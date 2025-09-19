d7eb45e281a858094c84560a6d6cad52e54ea335
Sydsilog <christopher.sy@cvsu.edu.ph>
2025-09-19 22:52:03 +0800

    NavBar for logged users

---
+++ src/components/Navlogged.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Avatar from "../assets/img/defAvatar.svg";
+import ChevDown from "../assets/img/chevronD.svg";
+
+const Navlogged = () => {
+  return (
+    <nav className=" w-full h-15 border-b-1 border-[rgba(0,0,0,0.25)] flex items-center pl-5 fixed z-50">
+      <div className="flex w-2/4">
+        <h1 className="poppins-extrabold text-3xl text-[#ff0000]">SPAID</h1>
+        <h1 className="poppins-extrabold text-3xl text-[#ff8c00]">LEARN</h1>
+      </div>
+      <div className="w-2/4  h-full flex justify-end items-center">
+        <a href="/Courses" className="poppins-regular">
+          Courses
+        </a>
+        <a
+          href="#"
+          className=" h-11 w-35 rounded-xl bg-[#f5f5f5] flex px-3 justify-between items-center mx-8"
+        >
+          {/* Learners Profile/Avatar */}
+          <img
+            src={Avatar}
+            alt="profile"
+            className="w-8 h-8 bg-white rounded-full"
+          />
+          <div className="flex flex-col justify-center items-center">
+            {/* Learners name */}
+            <h1>Sid</h1>
+            <p className="text-xs text-[#403F3F]">Learner</p>
+          </div>
+          <img src={ChevDown} alt="" className="w-3 h-3" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navlogged;
