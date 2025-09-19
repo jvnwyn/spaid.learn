2a5648d389e7a8b3cad4e4000a3eecea962233ea
Sydsilog <christopher.sy@cvsu.edu.ph>
2025-09-19 22:58:57 +0800

    Updated the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Navlogged from "./components/Navlogged";
+import LearnersCard from "./components/LearnersCard";
+import RecoCourse from "./components/RecoCourse";
+import Review from "./components/Review";
+import QrCom from "./components/QrCom";
 
 function App() {
   return (
     <>
-      <Login />
+      <Navlogged />
+
+      <div className="flex  ">
+        <div className="w-13/20 ">
+          <RecoCourse />
+          <Review />
+          <QrCom />
+        </div>
+        <div className="w-8/20 ">
+          <LearnersCard />
+        </div>
+      </div>
     </>
   );
 }
