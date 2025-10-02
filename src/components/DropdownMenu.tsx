888469f6384425b3f5b62d9427ad28f63ddc1212
Sydsilog <christopher.sy@cvsu.edu.ph>
2025-10-03 01:41:21 +0800

    added logout button

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 const DropdownMenu = () => {
-  return <div>DropdownMenu</div>;
+  const navigate = useNavigate();
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    navigate("/");
+  }
+  return (
+    <div className="absolute bg-white right-2 md:right-8 rounded-md border border-[rgba(0,0,0,0.25)] top-18  max-w-xs  shadow-lg z-50">
+      <ul className="p-2 md:p-3 flex flex-col gap-3 md:gap-5 items-center">
+        <li>
+          <Link
+            to=""
+            className="px-4 md:px-8 py-2 md:py-3 w-full  text-center rounded-md text-base md:text-lg"
+          >
+            PROFILE
+          </Link>
+        </li>
+        <li>
+          <button
+            className="cursor-pointer px-4 md:px-8 py-2 md:py-3 text-center rounded-md text-base md:text-lg"
+            onClick={signOut}
+          >
+            LOG OUT
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default DropdownMenu;
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,28 +1,33 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { data, Outlet, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
 import Navlogged from "../components/Navlogged";
-import type {User} from "@supabase/supabase-js";
+import type { User } from "@supabase/supabase-js";
 
 const MainLayout = () => {
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    supabase.auth.getUser().then(async ({ data}) => {
+    supabase.auth.getUser().then(async ({ data }) => {
       setUser(data.user);
 
       if (data.user) {
         navigate("/Home");
       }
 
+      if (!data.user) {
+        navigate("/");
+      }
+
       if (data.user) {
-        const { data: profile} = await supabase
+        const { data: profile } = await supabase
           .from("profiles")
           .select("*")
           .eq("id", data.user.id)
           .single();
-        
+        console.log(user);
+
         if (!profile) {
           await supabase.from("profiles").insert([
             {
@@ -31,14 +36,14 @@ const MainLayout = () => {
               role: "student",
             },
           ]);
-        }  
+        }
       }
     });
   }, []);
 
   return (
     <>
-      <Navlogged />
+      <Navlogged userData={user?.id} />
       <Outlet />
     </>
   );
